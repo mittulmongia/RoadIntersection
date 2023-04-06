84482f1aa985b6172c8d77cd85a8677658e67c79
mittulmongia <mittulmongia4@gmail.com>
2023-04-06 14:36:15 +0100

    Added Rough Implementation of Strategy pattern

--- CW1/src/ConcreteStrategyA.java
+++ CW1/src/ConcreteStrategyA.java
@@ -1,5 +1,6 @@
 public class ConcreteStrategyA implements Strategy {
-    public void executeStrategy(int input) {
+    // Rough Implementation of Strategy
+	public void executeStrategy(int input) {
         System.out.println("Executing Strategy A with input " + input);
     }
 }
