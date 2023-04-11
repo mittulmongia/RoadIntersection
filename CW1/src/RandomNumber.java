9b6e22029a12714a3e9b0ba0eef5655b1f78b049
mittulmongia <mittulmongia4@gmail.com>
2023-04-11 13:22:05 +0100

    Added the implementation of Random number generation

---
+++ CW1/src/RandomNumber.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class RandomNumber {
+
+	public static Random generateRandomSeed() {
+		Random random = new Random();
+//		random.setSeed(seed);
+		return random;
+	}
+
+}
